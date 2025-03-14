7d478c3e42793deeec606f2d89ee2e261d5ad444
Shakir Salim <80387319+shakir91@users.noreply.github.com>
2025-03-15 00:12:28 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,6 +6,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 
+hourly_url = "https://raw.githubusercontent.com/shakir91/Proyek_Analisis_Data_LaskarAI/main/data/hour.csv"
+hour_df = pd.read_csv(hourly_url)
 hourly_avg = hour_df.groupby('hour')['cnt'].mean()
 plt.figure(figsize=(12, 6))
 sns.lineplot(x=hourly_avg.index, y=hourly_avg.values, color='#2ca02c')
