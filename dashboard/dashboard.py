e9fb1914d1c93fd4a63d0fa84ebf63d601007357
Shakir Salim <80387319+shakir91@users.noreply.github.com>
2025-03-15 00:44:15 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,6 +8,13 @@ import streamlit as st
 
 hourly_url = "https://raw.githubusercontent.com/shakir91/Proyek_Analisis_Data_LaskarAI/main/data/hour.csv"
 hour_df = pd.read_csv(hourly_url)
+
+hourly_avg = hour_df.groupby('hour')['cnt'].mean()
+user_types = hour_df.groupby('hr')[['casual', 'registered']].mean()
+weekday_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+heatmap_data = hour_df.groupby(['weekday', 'hour'])['cnt'].mean().unstack()
+heatmap_data.describe(include=("all"))
+
 #Data Pre-Processing
 hour_df['dteday'] = pd.to_datetime(hour_df['dteday'])
 hour_df['datetime'] = hour_df['dteday'] + pd.to_timedelta(hour_df['hr'], unit='h')
@@ -16,6 +23,7 @@ hour_df['weekday'] = hour_df['datetime'].dt.day_name()
 hour_df['season'] = hour_df['season'].map({1: 'Spring', 2: 'Summer', 3: 'Fall', 4: 'Winter'})
 hour_df['weathersit'] = hour_df['weathersit'].map({1: 'Clear', 2: 'Mist', 3: 'Light Rain', 4: 'Heavy Rain'})
 hourly_avg = hour_df.groupby('hour')['cnt'].mean()
+
 plt.figure(figsize=(12, 6))
 sns.lineplot(x=hourly_avg.index, y=hourly_avg.values, color='#2ca02c')
 plt.title('Average Hourly Rentals Pattern', fontsize=14)
